ef81e04cc7bbcd56406c3c3715270e4c8dd71c71
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-20 21:32:17 +0100

    feat: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    background: './src/background.ts',
+    content: './src/content.ts',
+    popup: './src/popup.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  }
+};
+
