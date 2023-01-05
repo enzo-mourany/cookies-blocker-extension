beda363f3475d311fb0b9540719c7e74b3eddba0
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-05 14:05:28 +0100

    fix: var -> let

--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,15 @@
 function toggleCookieBlocking(shouldBlock) {
   chrome.storage.local.set({ blockCookies: shouldBlock });
   chrome.cookies.getAll({}, function(cookies) {
-    for (var i = 0; i < cookies.length; i++) {
+    for (let i = 0; i < cookies.length; i++) {
       chrome.cookies.remove({ url: "https://" + cookies[i].domain + cookies[i].path, name: cookies[i].name });
     }
   });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  var allowButton = document.getElementById("allow-button");
-  var blockButton = document.getElementById("block-button");
+  let allowButton = document.getElementById("allow-button");
+  let blockButton = document.getElementById("block-button");
   allowButton.addEventListener("click", function() {
     toggleCookieBlocking(false);
   });
